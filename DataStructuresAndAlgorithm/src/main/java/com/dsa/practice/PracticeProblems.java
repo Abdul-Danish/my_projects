266d51ebe2e425e09dc5634cbed503a27727217f
AbdulDanish <danish@digitaldots.ai>
2025-01-02 14:17:37 +0530

    added problem

--- DataStructuresAndAlgorithm/src/main/java/com/dsa/practice/PracticeProblems.java
+++ DataStructuresAndAlgorithm/src/main/java/com/dsa/practice/PracticeProblems.java
@@ -149,27 +149,45 @@ public class PracticeProblems {
         */
 		
 		/*
-		 * problem - 4: 
+		 * problem - 4: Minimum Cost For Tickets
 		 */
 	    
 	    
+	    /*
 	    // input
-	    int[] days = { 1, 4, 6, 7, 8, 20 }; 
-	    int[] costs = { 2, 7, 15 };
+        int[] days = { 1, 4, 6, 7, 8, 20 }; // expected: 11
+        int[] costs = { 2, 7, 15 };
 	    
-	    // logic
-	    int totalMinCost = 0;
-	    
-	    int l = 0;
-	    int r = days.length - 1;
-	    
-	    while (l < r) {
-	        
-	        for (int i=0; i<costs.length-1; i++) {
-	            
-	        }
-	    }
+        int maxDay = days[days.length-1];
+        boolean[] travelDays = new boolean[maxDay + 1];
+
+        for (int day : days) {
+            travelDays[day] = true;
+        }
+
+        int[] dp = new int[maxDay + 1];
+        dp[0] = 0;
+        for (int i=1; i<=maxDay; i++) {
+            if (!travelDays[i]) {
+                dp[i] = dp[i-1];
+                continue;
+            }
+
+            dp[i] = costs[0] + dp[i-1];
+            dp[i] = Math.min(dp[Math.max(0, i-7)] + costs[1], dp[i]);
+            dp[i] = Math.min(dp[Math.max(0, i-30)] + costs[2], dp[i]);
+        }
         
+        for (int i : dp) {
+            System.out.print(i + " ");
+        }
+        */
+	    
+	    /*
+         * problem - 5: 
+         */
+        
+	    
 	}
 
 }
