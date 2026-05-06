9e46207e709ba0dc65eab9362f6455ba833208ac
Danish <danish@digitaldots.ai>
2026-05-06 16:00:24 +0530

    remove project

--- connectors/function/src/main/java/com/digitaldots/connector/handler/Handler.java
+++
@@ -1,9 +0,0 @@
-package com.digitaldots.connector.handler;
-
-import java.util.Map;
-
-public interface Handler {
-
-     public Map<String, Object> handle(Map<String, Map<String, Object>> request, Map<String, Map<String, String>> headers);
-    
-}
